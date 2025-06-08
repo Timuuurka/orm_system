58f4560afd863d2305e23a91a28eca66ed58fe5f
Timuuurka <timuuurka.zh@gmail.com>
2025-06-09 00:37:21 +0500

    123

--- src/components/MapSearch.jsx
+++ src/components/MapSearch.jsx
@@ -17,25 +17,27 @@ const MapSearch = ({ onPlaceSelected }) => {
   };
 
   return (
-    <div className="w-[70%] max-w-4xl h-[60vh] ml-12 my-8 relative rounded-xl shadow-lg overflow-hidden">
-      <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY} libraries={libraries}>
-        <GoogleMap
-          mapContainerStyle={{ width: "100%", height: "100%" }}
-          center={center}
-          zoom={13}
-        >
-          <StandaloneSearchBox
-            onLoad={(ref) => (searchBoxRef.current = ref)}
-            onPlacesChanged={handlePlaceChanged}
+    <div className="overflow-x-hidden">
+      <div className="w-[70%] max-w-4xl h-[60vh] ml-[200px] my-8 relative rounded-xl shadow-lg overflow-hidden">
+        <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY} libraries={libraries}>
+          <GoogleMap
+            mapContainerStyle={{ width: "100%", height: "100%" }}
+            center={center}
+            zoom={13}
           >
-            <input
-              type="text"
-              placeholder="Введите название бизнеса"
-              className="absolute top-4 left-1/2 transform -translate-x-1/2 w-[280px] sm:w-[320px] h-10 px-4 text-sm border border-gray-300 rounded-md shadow-md z-10 focus:outline-none"
-            />
-          </StandaloneSearchBox>
-        </GoogleMap>
-      </LoadScript>
+            <StandaloneSearchBox
+              onLoad={(ref) => (searchBoxRef.current = ref)}
+              onPlacesChanged={handlePlaceChanged}
+            >
+              <input
+                type="text"
+                placeholder="Введите название бизнеса"
+                className="absolute top-4 left-1/2 transform -translate-x-1/2 w-[280px] sm:w-[320px] h-10 px-4 text-sm border border-gray-300 rounded-md shadow-md z-10 focus:outline-none"
+              />
+            </StandaloneSearchBox>
+          </GoogleMap>
+        </LoadScript>
+      </div>
     </div>
   );
 };
