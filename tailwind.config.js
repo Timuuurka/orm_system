e7d0e045cad67873da62be2521c47c2666fd37b2
Timuuurka <timuuurka.zh@gmail.com>
2025-06-08 22:34:42 +0500

    123
    132

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js,jsx,tsx}"], // Ensure Tailwind applies styles in JSX files
-    theme: {
-      extend: {
-        fontFamily: {
-          sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
-        },
-        colors: {
-          primary: {
-            DEFAULT: "#2563eb",
-            light: "#3b82f6",
-            dark: "#1e40af",
-          },
-          dark: {
-            bg: "#0f172a",
-            card: "#1e293b",
-            text: "#f8fafc",
-          },
-        },
-      },
-    },
-    plugins: [],
-  };
-  
+  content: [
+    "./index.html",         // для Vite
+    "./src/**/*.{js,jsx}",  // для всех твоих компонентов и страниц
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
