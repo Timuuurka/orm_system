9f1f1a867ec3b56dd4f2fb679ecbf5aa5eddd29d
Timuuurka <timuuurka.zh@gmail.com>
2025-06-09 00:21:43 +0500

    123
    123

--- src/components/MapSearch.jsx
+++ src/components/MapSearch.jsx
@@ -1,4 +1,3 @@
-// src/components/MapSearch.jsx
 import React, { useRef, useState } from "react";
 import { GOOGLE_MAPS_API_KEY } from "../config";
 import { GoogleMap, LoadScript, StandaloneSearchBox } from "@react-google-maps/api";
@@ -18,31 +17,41 @@ const MapSearch = ({ onPlaceSelected }) => {
   };
 
   return (
-    <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY} libraries={libraries}>
-      <GoogleMap mapContainerStyle={{ width: "100%", height: "400px" }} center={center} zoom={13}>
-        <StandaloneSearchBox onLoad={(ref) => (searchBoxRef.current = ref)} onPlacesChanged={handlePlaceChanged}>
-          <input
-            type="text"
-            placeholder="Введите название бизнеса"
-            style={{
-              boxSizing: `border-box`,
-              border: `1px solid transparent`,
-              width: `240px`,
-              height: `40px`,
-              padding: `0 12px`,
-              marginTop: "10px",
-              borderRadius: `4px`,
-              fontSize: `14px`,
-              outline: `none`,
-              textOverflow: `ellipses`,
-              position: "absolute",
-              left: "50%",
-              transform: "translateX(-50%)",
-            }}
-          />
-        </StandaloneSearchBox>
-      </GoogleMap>
-    </LoadScript>
+    <div style={{ position: "relative", width: "100%", height: "600px" }}>
+      <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY} libraries={libraries}>
+        <GoogleMap
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          center={center}
+          zoom={13}
+        >
+          <StandaloneSearchBox
+            onLoad={(ref) => (searchBoxRef.current = ref)}
+            onPlacesChanged={handlePlaceChanged}
+          >
+            <input
+              type="text"
+              placeholder="Введите название бизнеса"
+              style={{
+                boxSizing: "border-box",
+                border: "1px solid transparent",
+                width: "300px",
+                height: "40px",
+                padding: "0 12px",
+                marginTop: "10px",
+                borderRadius: "4px",
+                fontSize: "14px",
+                outline: "none",
+                position: "absolute",
+                top: "10px",
+                left: "50%",
+                transform: "translateX(-50%)",
+                zIndex: 5,
+              }}
+            />
+          </StandaloneSearchBox>
+        </GoogleMap>
+      </LoadScript>
+    </div>
   );
 };
 
