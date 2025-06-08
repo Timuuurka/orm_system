a793be8b543d8e7ac2bbb0c762b7db7af13f39b6
Timuuurka <timuuurka.zh@gmail.com>
2025-06-08 16:44:50 +0500

    123
    123

--- src/services/sentiment.js
+++ src/services/sentiment.js
@@ -1,28 +1,24 @@
 import { HUGGINGFACE_API_KEY  } from "../config";
 // services/sentiment.js
-export async function analyzeSentiment(text) {
-  const response = await fetch("https://api-inference.huggingface.co/tabularisai/multilingual-sentiment-analysis", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${HUGGINGFACE_API_KEY }`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ inputs: text }),
-  });
+export const analyzeSentiment = async (text) => {
+
+  const response = await fetch(
+    "https://router.huggingface.co/hf-inference/models/tabularisai/multilingual-sentiment-analysis",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${HUGGINGFACE_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ inputs: text }),
+    }
+  );
 
   if (!response.ok) {
-    throw new Error(`Hugging Face API error: ${response.statusText}`);
+    const errorText = await response.text();
+    throw new Error(`Hugging Face API error: ${errorText}`);
   }
 
   const result = await response.json();
-
-  if (!Array.isArray(result) || !result[0]) {
-    return "unknown";
-  }
-
-  const label = result[0][0].label.toLowerCase(); // "POSITIVE", "NEUTRAL", "NEGATIVE"
-
-  return label === "positive" || label === "neutral" || label === "negative"
-    ? label
-    : "unknown";
-}
+  return result;
+};
