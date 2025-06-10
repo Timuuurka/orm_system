d8feae86e0fac5d1eb19d5dcda1a85a9de77915b
Timuuurka <timuuurka.zh@gmail.com>
2025-06-11 03:40:56 +0500

    123
    23

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./components/PrivateRoute";
-
-// Новые страницы
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Blog from "./pages/Blog";
 import Terms from "./pages/Terms";
 import Privacy from "./pages/Privacy";
 import NotFound from "./pages/NotFound";
-import Reports from "./pages/ReportsPage";
-import Reviews from "./pages/Reviews";
 
 function App() {
   return (
@@ -34,16 +30,6 @@ function App() {
       <Route path="/" element={
         <PrivateRoute>
           <Dashboard />
-        </PrivateRoute>
-      } />
-      <Route path="/reports" element={
-        <PrivateRoute>
-          <Reports />
-        </PrivateRoute>
-      } />
-      <Route path="/reviews" element={
-        <PrivateRoute>
-          <Reviews />
         </PrivateRoute>
       } />
       {/* 404 */}
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
 
 const MainLayout = ({ children }) => (
   <div className="flex flex-col min-h-screen">
     <Header />
-    <Navbar />
     <main className="flex-grow px-4 py-6 max-w-6xl mx-auto">
       {children}
     </main>
